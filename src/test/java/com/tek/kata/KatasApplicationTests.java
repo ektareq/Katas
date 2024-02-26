ba864b8bdaae5b2ea653e603cb8cbb994d8ab820
Tareq <Tareq@gmail.com>
2024-02-26 19:56:39 +0100

    first commit

---
+++ src/test/java/com/tek/kata/KatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tek.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
